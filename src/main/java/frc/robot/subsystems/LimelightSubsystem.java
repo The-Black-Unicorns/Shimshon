837a11d6c526adef339e7676b33ac6800d356963
Black Unicorns <5135blackunicorns@gmail.com>
2022-04-30 23:56:24 +0300

    started limlight subsystem

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -4,16 +4,49 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimelightSubsystem extends SubsystemBase {
-  /** Creates a new LimelightSubsystem. */
-  public LimelightSubsystem() {
-    
+  
+  private static LimelightSubsystem limelightSubsystemInstance;
+
+  private NetworkTableEntry tvEntry; //Valid targets
+  private NetworkTableEntry txEntry; //X angle
+  private NetworkTableEntry tyEntry; //Y angle
+  private NetworkTableEntry tlEntry; //Latency
+  
+  private LimelightSubsystem() {
+    NetworkTable limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
+
+    tvEntry = limelightTable.getEntry("tv");
+    txEntry = limelightTable.getEntry("tx");
+    tyEntry = limelightTable.getEntry("ty");
+    tlEntry = limelightTable.getEntry("tl");
+  }
+
+  public static LimelightSubsystem getInstance(){
+    if (limelightSubsystemInstance == null)
+      limelightSubsystemInstance = new LimelightSubsystem();
+
+    return limelightSubsystemInstance;
+  }
+
+  public boolean hasTarget(){
+    if (tvEntry.getDouble(0) == 0){
+      return false;
+    } else {
+      return true;
+    }
+  } 
+
+  public double getXAngle(){
+    return txEntry.getDouble(0);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
   }
 }
