ba687d826d89112865138aa71ddf184400d4ad40
cookthecookie <71383536+cookthecookie@users.noreply.github.com>
2022-08-06 12:16:04 +0300

    maybe finished steer motor class

--- src/main/java/com/blackunicornsswerve/ModuleInfo.java
+++ src/main/java/com/blackunicornsswerve/ModuleInfo.java
@@ -10,11 +10,13 @@ public class ModuleInfo {
     private final double wheelDiameter;
     private final double gearRatio;
     private final double maxSpeed;
+    private final double steerReduction;
 
-    public ModuleInfo (double wheelDiameter, double gearRatio, double maxSpeed){
+    public ModuleInfo (double wheelDiameter, double gearRatio, double maxSpeed, double steerReduction){
         this.wheelDiameter = wheelDiameter;
         this.gearRatio = gearRatio;
         this.maxSpeed = maxSpeed;
+        this.steerReduction = steerReduction;
     }
 
     public double getGearRatio(){
@@ -29,9 +31,13 @@ public class ModuleInfo {
         return maxSpeed;
     }
 
+    public double getSteerReduction (){
+        return steerReduction;
+    }
 
-    public ModuleInfo Mk4_L1 = new ModuleInfo(0.1016, 1/8.14,4.118);
-    public ModuleInfo Mk4_L2 = new ModuleInfo(0.1016, 1/6.75,4.9682);
-    public ModuleInfo Mk4_L3 = new ModuleInfo(0.1016, 1/6.12,5.486);
-    public ModuleInfo Mk4_L4 = new ModuleInfo(0.1016, 1/5.14,6.522);
+
+    public ModuleInfo Mk4_L1 = new ModuleInfo(0.1016, 1/8.14,4.118, 1/12.8);
+    public ModuleInfo Mk4_L2 = new ModuleInfo(0.1016, 1/6.75,4.9682, 1/12.8);
+    public ModuleInfo Mk4_L3 = new ModuleInfo(0.1016, 1/6.12,5.486, 1/12.8);
+    public ModuleInfo Mk4_L4 = new ModuleInfo(0.1016, 1/5.14,6.522, 1/12.8);
 }
--- src/main/java/com/blackunicornsswerve/SteerMotor.java
+++ src/main/java/com/blackunicornsswerve/SteerMotor.java
@@ -4,6 +4,7 @@
 
 package com.blackunicornsswerve;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.sensors.AbsoluteSensorRange;
 import com.ctre.phoenix.sensors.CANCoder;
@@ -18,13 +19,18 @@ public class SteerMotor {
     private TalonFX motor;
     private CANCoder encoder;
     private ModuleInfo moduleInfo;
-    public Rotation2d steerOffset;
+    private Rotation2d steerOffset;
+
+    private double sensorPositionCoefficient;
+    private int resetIteration = 0;
 
     public SteerMotor (int motorCANid, int encoderCANid, ModuleInfo moduleInfo, double steerOffsetRadians) {
         this.moduleInfo = moduleInfo;
         encoder = new CANCoder(encoderCANid);
         motor = new TalonFX(motorCANid);
         steerOffset = new Rotation2d(steerOffsetRadians);
+
+        sensorPositionCoefficient = 2.0 * Math.PI / 2048 * this.moduleInfo.getSteerReduction();
 
         configureEncoder(encoder, steerOffset);        
     }
@@ -38,6 +44,46 @@ public class SteerMotor {
         CtreUtils.checkCtreError(encoder.configAllSettings(config, 250), "Failed To Configure CANCoder");
     }
 
+    public void setReferenceAngle(double referenceAngleRadians){
+
+
+        //Setting Falcon encoder equal to absolute encoder 10 seconds after startup
+        resetIteration++;
+        if (resetIteration == 500){
+            double absoluteAngleRadians = Math.toRadians(encoder.getAbsolutePosition());
+            absoluteAngleRadians %= (2*Math.PI);
+            if (absoluteAngleRadians <0){
+                absoluteAngleRadians += 2*Math.PI;
+            }
+            motor.setSelectedSensorPosition(absoluteAngleRadians /sensorPositionCoefficient);
+        }
+
+
+        double currentAngleRadians = motor.getSelectedSensorPosition() * sensorPositionCoefficient;
+
+        double currentAngleRadiansMod = currentAngleRadians % (2*Math.PI);
+        if (currentAngleRadiansMod < 0){
+            currentAngleRadians += 2*Math.PI;
+        }
+
+        double adjustedReferenceAngleRadians = referenceAngleRadians + currentAngleRadians - currentAngleRadiansMod;
+        if (referenceAngleRadians - currentAngleRadiansMod > Math.PI) {
+            adjustedReferenceAngleRadians -= 2.0 * Math.PI;
+        } else if (referenceAngleRadians - currentAngleRadiansMod < -Math.PI) {
+            adjustedReferenceAngleRadians += 2.0 * Math.PI;
+        }
+
+        motor.set(ControlMode.Position, adjustedReferenceAngleRadians /sensorPositionCoefficient);
+    }
+
+    public double getSteerAngle(){
+        double currentAngleRadians = motor.getSelectedSensorPosition() * sensorPositionCoefficient;
+        double currentAngleRadiansMod = currentAngleRadians % (2*Math.PI);
+        if (currentAngleRadiansMod < 0){
+            currentAngleRadians += 2*Math.PI;
+        } 
+        return currentAngleRadiansMod;
+    }
 
 
 }
