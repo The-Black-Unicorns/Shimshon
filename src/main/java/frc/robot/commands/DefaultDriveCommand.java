71a222fb37923abf5ed74ace72f6d58cd737f937
Yonatan Avhar <yonatan3058@outlook.com>
2022-10-13 08:36:06 +0300

    Emergency

--- src/main/java/frc/robot/commands/DefaultDriveCommand.java
+++ src/main/java/frc/robot/commands/DefaultDriveCommand.java
@@ -9,7 +9,6 @@ import frc.robot.Constants;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.GyroSubsystem;
 
-
 public class DefaultDriveCommand extends CommandBase {
 
     private final DrivetrainSubsystem m_drivetrainSubsystem;
@@ -19,12 +18,13 @@ public class DefaultDriveCommand extends CommandBase {
     private boolean isFieldOriented;
     private boolean isAlternateDriveControl;
 
-    //Max acceleration m/s^2
+    // Max acceleration m/s^2
     SlewRateLimiter Xfilter = new SlewRateLimiter(6);
     SlewRateLimiter Yfilter = new SlewRateLimiter(6);
     SlewRateLimiter speedLimiter = new SlewRateLimiter(1000);
 
-    public DefaultDriveCommand(DrivetrainSubsystem drivetrainSubsystem, Joystick controller, PS4Controller alternateController) {
+    public DefaultDriveCommand(DrivetrainSubsystem drivetrainSubsystem, Joystick controller,
+            PS4Controller alternateController) {
         this.m_drivetrainSubsystem = drivetrainSubsystem;
         input = controller;
         alternateDriveController = alternateController;
@@ -32,49 +32,57 @@ public class DefaultDriveCommand extends CommandBase {
 
         changePID();
     }
-    // @Config (tabName = Constants.MAIN_DASHBOARD_TAB_NAME, name = "Field Oriented", defaultValueBoolean = true)
-    public void setFieldOriented(boolean value){
+
+    // @Config (tabName = Constants.MAIN_DASHBOARD_TAB_NAME, name = "Field
+    // Oriented", defaultValueBoolean = true)
+    public void setFieldOriented(boolean value) {
         isFieldOriented = value;
     }
 
-    // @Config (tabName = Constants.MAIN_DASHBOARD_TAB_NAME, name = "Alternate Drive", defaultValueBoolean = false)
-    public void setAlternateDrive(boolean value){
+    // @Config (tabName = Constants.MAIN_DASHBOARD_TAB_NAME, name = "Alternate
+    // Drive", defaultValueBoolean = false)
+    public void setAlternateDrive(boolean value) {
         isAlternateDriveControl = value;
     }
 
-    
     @Override
     public void execute() {
         ChassisSpeeds inputSpeed;
-        if(!Constants.ALTERNATE_DRIVE){
+        if (!Constants.ALTERNATE_DRIVE) {
             double sensitivity = input.getRawAxis(4) / 2 + 0.5;
             // sensitivity = 0.25;
-             inputSpeed = new ChassisSpeeds(
-                Xfilter.calculate(deadband(input.getRawAxis(Constants.DRIVER_CONTROLLER_X_AXIS_ID), 0.05) * sensitivity * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND),
-                Yfilter.calculate(deadband(-input.getRawAxis(Constants.DRIVER_CONTROLLER_Y_AXIS_ID), 0.05) * sensitivity * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND),
-                deadband(-input.getRawAxis(Constants.DRIVER_CONTROLLER_Z_AXIS_ID), 0.05) * sensitivity * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND);
+            inputSpeed = new ChassisSpeeds(
+                    Xfilter.calculate(deadband(input.getRawAxis(Constants.DRIVER_CONTROLLER_X_AXIS_ID), 0.05)
+                            * sensitivity * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND),
+                    Yfilter.calculate(deadband(-input.getRawAxis(Constants.DRIVER_CONTROLLER_Y_AXIS_ID), 0.05)
+                            * sensitivity * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND),
+                    deadband(-input.getRawAxis(Constants.DRIVER_CONTROLLER_Z_AXIS_ID), 0.05) * sensitivity
+                            * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND);
             // System.out.println(inputSpeed.toString());
-    
-        }
-        else{
+
+        } else {
             double xInput = deadband(alternateDriveController.getRightX(), 0.1);
             double yInput = deadband(alternateDriveController.getRightY(), 0.1);
             double angle = Math.atan2(yInput, xInput);
-            double speed = speedLimiter.calculate(Math.pow(((alternateDriveController.getR2Axis()+1)/2)* DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND, 1))  ;
-            if (xInput == 0 && yInput == 0){
+            double speed = speedLimiter.calculate(Math.pow(((alternateDriveController.getR2Axis() + 1) / 2)
+                    * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND, 1));
+            if (xInput == 0 && yInput == 0) {
                 speed = 0;
             }
             // System.out.println("Test " + speed);
-            double rotation = deadband(-alternateDriveController.getLeftX(),0.1)*DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND;
-            double xSpeed = Xfilter.calculate(-Math.cos(angle)*speed);
-            double ySpeed = Yfilter.calculate(-Math.sin(angle)*speed);
-            inputSpeed = new ChassisSpeeds(ySpeed,xSpeed,rotation);
+            double rotation = deadband(-alternateDriveController.getLeftX(), 0.1)
+                    * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND;
+            double xSpeed = Xfilter.calculate(-Math.cos(angle) * speed);
+            double ySpeed = Yfilter.calculate(-Math.sin(angle) * speed);
+            inputSpeed = new ChassisSpeeds(ySpeed, xSpeed, rotation);
         }
 
         if (!input.getRawButton(5)) {
             if (!input.getRawButton(1)) {
                 m_drivetrainSubsystem.drive(
-                        ChassisSpeeds.fromFieldRelativeSpeeds(inputSpeed.vxMetersPerSecond, inputSpeed.vyMetersPerSecond, inputSpeed.omegaRadiansPerSecond, GyroSubsystem.getInstance().getGyroscopeRotation()));
+                        ChassisSpeeds.fromFieldRelativeSpeeds(inputSpeed.vxMetersPerSecond,
+                                inputSpeed.vyMetersPerSecond, inputSpeed.omegaRadiansPerSecond,
+                                GyroSubsystem.getInstance().getGyroscopeRotation()));
             } else {
                 m_drivetrainSubsystem.drive(inputSpeed);
             }
@@ -94,7 +102,7 @@ public class DefaultDriveCommand extends CommandBase {
         if (Math.abs(value) < lowerLimit) {
             return 0;
         } else {
-            
+
             if (value > 0) {
                 return (value - lowerLimit) / (1 - lowerLimit);
             } else {
